7d39dbeef21e3ae93ad8ee6d4125e67833e67424
Kristen Krofchik <k.krofchik@gmail.com>
2021-03-11 14:53:24 -0600

    setting up js file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/mean', (req, res) => {
+
+})
+
+app.get('/median', (req, res) => {
+
+})
+
+app.get('/mode', (req, res) => {
+    
+}) 
+
+
+app.listen(3000, function() {
+    console.log('App on Port 3000')
+});
